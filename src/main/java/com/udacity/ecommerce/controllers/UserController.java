ca421c7410ac321c7ee6d7243773bc00cafce2f8
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-11-20 19:48:33 -0300

    Add logging

--- src/main/java/com/udacity/ecommerce/controllers/UserController.java
+++ src/main/java/com/udacity/ecommerce/controllers/UserController.java
@@ -5,52 +5,58 @@ import com.udacity.ecommerce.model.persistence.User;
 import com.udacity.ecommerce.model.persistence.repositories.CartRepository;
 import com.udacity.ecommerce.model.persistence.repositories.UserRepository;
 import com.udacity.ecommerce.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
-	private final UserRepository userRepository;
-	
-	private final CartRepository cartRepository;
 
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
 
-	public UserController(UserRepository userRepository, CartRepository cartRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.userRepository = userRepository;
-		this.cartRepository = cartRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
+    private final UserRepository userRepository;
 
-	@GetMapping("/id/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		return ResponseEntity.of(userRepository.findById(id));
-	}
-	
-	@GetMapping("/{username}")
-	public ResponseEntity<User> findByUserName(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
-	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
-		Cart cart = new Cart();
-		cartRepository.save(cart);
-		user.setCart(cart);
-		String password = createUserRequest.getPassword();
-		String confirmPassword = createUserRequest.getConfirmPassword();
-		if (password.length() < 8 || !password.equals(confirmPassword)) {
-			return ResponseEntity.badRequest().build();
-		}
-		user.setPassword(bCryptPasswordEncoder.encode(password));
-		userRepository.save(user);
-		return ResponseEntity.ok(user);
-	}
-	
+    private final CartRepository cartRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository, CartRepository cartRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.cartRepository = cartRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.of(userRepository.findById(id));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        User user = new User();
+        user.setUsername(createUserRequest.getUsername());
+        log.info("User name set with {}", createUserRequest.getUsername());
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        user.setCart(cart);
+        String password = createUserRequest.getPassword();
+        String confirmPassword = createUserRequest.getConfirmPassword();
+        if (password.length() < 8 || !password.equals(confirmPassword)) {
+            return ResponseEntity.badRequest().build();
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
+    }
+
 }
